294c9fd940c9f0fac5a33461500d348f96fd94ee
Theophilus005 <theophilito@gmail.com>
2021-06-16 04:38:11 +0000

    project3

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Project 3',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    launch("http://pms12.byethost7.com/index.php",
+        forceWebView: true, enableJavaScript: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Container());
+  }
+}
